d2d9bd1f231cde976fd5792d675d51620e9fbf66
mujiburrohman <muji.official06@gmail.com>
2023-10-31 23:58:06 +0700

    feat: darkmode switch

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,12 +6,14 @@ import React from "react";
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav>
+      <nav className="flex justify-between border-b border-border h-14 px-4 py-2">
         <Logo />
-        <ThemeSwitcher />
-        <UserButton afterSignOutUrl="/sign-in" />
+        <div className="flex gap-4 items-center">
+          <ThemeSwitcher />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
       </nav>
-      <main className="flex w-full flex-grow bg-red-300">{children}</main>
+      <main className="flex w-full flex-grow">{children}</main>
     </div>
   );
 }
--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,8 +1,40 @@
 "use client";
-import React from "react";
+import { useTheme } from "next-themes";
+import React, { useEffect, useState } from "react";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
 
 function ThemeSwitcher() {
-  return <div>ThemeSwitcher</div>;
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Tabs defaultValue={theme}>
+      <TabsList>
+        <TabsTrigger
+          value="light"
+          onClick={() => {
+            setTheme("light");
+          }}
+        >
+          Light
+        </TabsTrigger>
+        <TabsTrigger
+          value="dark"
+          onClick={() => {
+            setTheme("dark");
+          }}
+        >
+          Dark
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
 }
 
 export default ThemeSwitcher;
